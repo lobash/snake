a1429246f4d665186930ba1df1376fb2663ce517
lobash <zhenyacitx@yandex.ru>
2018-01-05 00:28:33 +0400

    Убрал логику из класса Area заголовочного файла

--- Area.cpp
+++ Area.cpp
@@ -1,10 +1,43 @@
 #include "Area.h"
 
+char *Area::simbolBorder = "#";
 
-	
-Area::Area(int width, int height) {
-	Area::width = width;
-	Area::height = height;
+void writeHorizontalLine(int width)  {
+	width = width+1;
+	for (int i = 0; i < width; i++) {
+		cout << *Area::simbolBorder;
+	}
+	cout << endl;			
 }
+
+void writeVerticalLines(int width, int height) {
+	for (int i=0; i< height; i++) {
+		for (int j=0; j< width; j++) {
+			if (j == 0 || j == width-1) {
+				cout << *Area::simbolBorder;
+			}
+			
+			cout << " ";
+		}
+		cout << endl;
+	}			
+}
+	
+Area::Area(int width, int height)
+	:width(width),height(height) {}
 		
+void Area::clearScreen() {
+	system("cls");
+}
+
+void Area::createBorder() {
+	writeHorizontalLine(width);
+	writeVerticalLines(width, height);
+	writeHorizontalLine(width);
+}
+
+void Area::createArea() {
+	clearScreen();
+	createBorder();
+}
 
--- Area.h
+++ Area.h
@@ -3,8 +3,6 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <string>
-
 
 using namespace std;
 
@@ -12,46 +10,12 @@ class Area {
 	
 	private :
 		int width, height;
-		const string _SIMBOL = "#";
-		void clearScreen() {
-			system("cls");
-		}
-		
-		void writeHorizontalLine()  {
-			for (int i = 0; i < width; i++) {
-				cout << this->_SIMBOL;
-			}
-			cout << endl;			
-		}
-		
-		void writeVerticalLines() {
-			for (int i=0; i< height; i++) {
-				for (int j=0; j< width; j++) {
-					if (j == 0 || j == width-1) {
-						cout << _SIMBOL;
-					}
-					
-					cout << " ";
-				}
-				cout << endl;
-			}			
-		}
-		
-		void createBorder() {
-			writeHorizontalLine();
-			writeVerticalLines();
-			writeHorizontalLine();
-		}
-	
-
+		void clearScreen();
+		void createBorder();
 		
 	public:
+		static char *simbolBorder;
 		Area(int width, int height);
-		void createArea() {
-			clearScreen();
-			createBorder();
-		}
-		
+		void createArea();
 };
-
 #endif
